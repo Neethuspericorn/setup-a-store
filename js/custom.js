d3eb32e1da991bfdcfa5bd010d8fabb725cfbf24
Anooperajendran <anoop.rajendran@spericorn.com>
2023-04-04 18:40:00 +0530

    product View page

--- js/custom.js
+++ js/custom.js
@@ -17,6 +17,47 @@ $(document).ready(function(){
     arrows: true,
     dots: false,
     pauseOnHover: false,
-});
+  });
 
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    focusOnSelect: true,
+    vertical: true,
+    responsive: [{
+      breakpoint: 1380,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 3,
+      }
+    },]
+  });
+
+  $('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+  });
+  
 });
